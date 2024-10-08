e0160c4979247d2aed5ba72597617a8e80fa6583
Bharath <bk3604073@gmail.com>
2024-10-08 08:14:46 +0530

    create apis for serviceProvider

---
+++ src/routes/serviceProviderRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const serviceProviderController = require('../controllers/serviceProviderController');
+
+// Routes for CRUD operations
+router.post('/add-service', serviceProviderController.createServiceProvider);
+router.get('/', serviceProviderController.getAllServiceProviders);
+router.get('/:id', serviceProviderController.getServiceProviderById);
+router.put('/:id', serviceProviderController.updateServiceProvider);
+router.delete('/:id', serviceProviderController.deleteServiceProvider);
+
+// Booking a slot
+router.post('/:id/book', serviceProviderController.bookSlot);
+
+// Reviews
+router.get('/:id/reviews', serviceProviderController.getReviews);
+router.post('/:id/reviews', serviceProviderController.addReview);
+
+module.exports = router;
