4e365c0b481ea2d578e12fdfce741a7c03edbb2c
Bharath <bk3604073@gmail.com>
2024-10-19 17:09:21 +0530

    spelling corrected

--- src/app.js
+++ src/app.js
@@ -57,6 +57,6 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Socket.IO server running on http://localhost:${PORT}`);
 });
