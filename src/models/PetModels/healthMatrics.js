00ebd80c66eb2f7e01689b881531dd313f8d2e8b
Bharath <bk3604073@gmail.com>
2024-10-19 16:31:30 +0530

    pushing

--- src/models/PetModels/healthMatrics.js
+++ src/models/PetModels/healthMatrics.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define healthMetrics schema
-const HealthMetricsSchema = new Schema({
+const healthMetricsSchema = new Schema({
     weight: { type: Number, required: true, min: 0 },
     height: { type: Number, required: true, min: 0 },
     date: { type: Date, default: Date.now }
 }, { _id: false });
 
-module.exports = mongoose.model('HealthMetrics', HealthMetricsSchema);
+module.exports = mongoose.model('HealthMetrics', healthMetricsSchema);
